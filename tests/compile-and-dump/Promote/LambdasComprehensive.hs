f15d00a238e4e0c47570b30dc04caa1afc288a5c
Jan Stolarek <jan.stolarek@p.lodz.pl>
2014-03-07 17:26:17 +0100

    Improve LambdasComprehensive test

--- tests/compile-and-dump/Promote/LambdasComprehensive.hs
+++ tests/compile-and-dump/Promote/LambdasComprehensive.hs
@@ -3,6 +3,8 @@ module Promote.LambdasComprehensive where
 import Data.Singletons.TH
 import Data.Singletons.Prelude
 
+--data Proxy a = Proxy
+
 $(promote [d|
  data Nat = Zero | Succ Nat
 
@@ -12,4 +14,20 @@ $(promote [d|
 
  foo :: [Nat]
  foo = map (\x -> either_ pred Succ x) [Left Zero, Right (Succ Zero)]
+
+ -- this is the same as above except that it does not use lambdas
+ bar :: [Nat]
+ bar = map (either_ pred Succ) [Left Zero, Right (Succ Zero)]
  |])
+
+fooTest1a :: Proxy Foo
+fooTest1a = Proxy
+
+fooTest1b :: Proxy [Zero, Succ (Succ Zero)]
+fooTest1b = fooTest1a
+
+barTest1a :: Proxy Bar
+barTest1a = Proxy
+
+barTest1b :: Proxy [Zero, Succ (Succ Zero)]
+barTest1b = barTest1a
