4a613a3b71d45acccb1f5040dbcfce669b5ac392
Jan Stolarek <jan.stolarek@p.lodz.pl>
2014-03-18 14:10:05 +0100

    Some more tests

--- tests/compile-and-dump/Promote/TopLevelPatterns.hs
+++ tests/compile-and-dump/Promote/TopLevelPatterns.hs
@@ -2,13 +2,30 @@ module Promote.TopLevelPatterns where
 
 import Data.Singletons.TH
 import Data.Singletons.List
+import Prelude hiding (Bool(..))
 
 $(promote [d|
   data Bool = False | True
+  data Foo = Bar (Bool -> Bool) (Bool -> Bool)
+ |])
 
+$(promote [d|
   otherwise :: Bool
   otherwise = True
 
+  id :: a -> a
+  id x = x
+
+  not :: Bool -> Bool
+  not True  = False
+  not False = True
+
   f,g :: Bool
   [f,g] = [otherwise, otherwise]
+
+  h,i :: Bool -> Bool
+  (h,i) = (not, id)
+
+  j,k :: Bool -> Bool
+  (Bar j k) = Bar not id
  |])
