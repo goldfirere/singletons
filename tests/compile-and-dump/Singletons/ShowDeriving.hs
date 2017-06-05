852407b6ecaab1a40ae711c95d8244d15c2596a4
Ryan Scott <ryan.gl.scott@gmail.com>
2017-06-05 19:18:55 -0400

    Fix tests

--- tests/compile-and-dump/Singletons/ShowDeriving.hs
+++ tests/compile-and-dump/Singletons/ShowDeriving.hs
@@ -18,20 +18,20 @@ $(singletons [d|
 
     |])
 
-foo1 :: "MkFoo1" :~: Show' MkFoo1
+foo1 :: "MkFoo1" :~: Show_ MkFoo1
 foo1 = Refl
 
 foo2a :: "(MkFoo2a LT GT)" :~: ShowsPrec 11 (MkFoo2a LT GT) ""
 foo2a = Refl
 
-foo2b :: "True `MkFoo2b` False" :~: Show' (True `MkFoo2b` False)
+foo2b :: "True `MkFoo2b` False" :~: Show_ (True `MkFoo2b` False)
 foo2b = Refl
 
-foo2c :: "(:*:) () ()" :~: Show' ('() :*: '())
+foo2c :: "(:*:) () ()" :~: Show_ ('() :*: '())
 foo2c = Refl
 
 foo2d :: "(False :&: True)" :~: ShowsPrec 10 (False :&: True) ""
 foo2d = Refl
 
-foo3 :: "MkFoo3 {getFoo3a = True, (***) = False}" :~: Show' (MkFoo3 True False)
+foo3 :: "MkFoo3 {getFoo3a = True, (***) = False}" :~: Show_ (MkFoo3 True False)
 foo3 = Refl
