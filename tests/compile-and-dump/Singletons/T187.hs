a307126f4648cba9d43b1c082584222efbd07999
Ryan Scott <ryan.gl.scott@gmail.com>
2017-07-08 20:04:52 -0400

    Make the #187 test less hacky
    Now that we support standalone deriving (see #194), we needn't jump through so
    many hoops in the T187 test.

--- tests/compile-and-dump/Singletons/T187.hs
+++ tests/compile-and-dump/Singletons/T187.hs
@@ -1,12 +1,8 @@
 module T187 where
 
-import Language.Haskell.TH
-import Data.Singletons.Prelude
 import Data.Singletons.TH
 
-data Empty
-
--- We need to construct the TH AST for Empty directly for now, as singletons
--- doesn't support standalone deriving declarations
-$(singletonsOnly (return [DataD [] ''Empty [] Nothing []
-                            [DerivClause Nothing [ConT ''Eq, ConT ''Ord]]]))
+$(singletons[d| data Empty
+                deriving instance Eq Empty
+                deriving instance Ord Empty
+              |])
