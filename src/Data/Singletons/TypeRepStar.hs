c72db69e10f3741a4247f4b196f8fe028beac860
Ryan Scott <ryan.gl.scott@gmail.com>
2017-07-23 13:56:18 -0400

    Rework Data.Singletons.TypeRepStar to use type-indexed Typeable

--- src/Data/Singletons/TypeRepStar.hs
+++ src/Data/Singletons/TypeRepStar.hs
@@ -12,75 +12,71 @@
 -- Stability   :  experimental
 -- Portability :  non-portable
 --
--- This module defines singleton instances making 'Typeable' the singleton for
+-- This module defines singleton instances making 'TypeRep' the singleton for
 -- the kind @*@. The definitions don't fully line up with what is expected
 -- within the singletons library, so expect unusual results!
 --
 ----------------------------------------------------------------------------
 
 module Data.Singletons.TypeRepStar (
-  Sing(STypeRep)
+  Sing(STypeRep),
   -- | Here is the definition of the singleton for @*@:
   --
-  -- > data instance Sing (a :: *) where
-  -- >   STypeRep :: Typeable a => Sing a
+  -- > newtype instance Sing (a :: *) where
+  -- >   STypeRep :: TypeRep a -> Sing a
   --
   -- Instances for 'SingI', 'SingKind', 'SEq', 'SDecide', and 'TestCoercion' are
   -- also supplied.
+
+  SomeTypeRepStar(..)
   ) where
 
 import Data.Singletons.Prelude.Instances
 import Data.Singletons.Internal
 import Data.Singletons.Prelude.Eq
-import Data.Typeable
-import Unsafe.Coerce
 import Data.Singletons.Decide
+import Type.Reflection
+import Unsafe.Coerce
 
 import Data.Kind
-import GHC.Exts ( Proxy# )
 import Data.Type.Coercion
 import Data.Type.Equality
 
-data instance Sing (a :: *) where
-  STypeRep :: Typeable a => Sing a
+newtype instance Sing (a :: *) where
+  STypeRep :: TypeRep a -> Sing a
+
+-- | A variant of 'SomeTypeRep' whose underlying 'TypeRep' is restricted to
+-- kind @*@.
+data SomeTypeRepStar where
+  SomeTypeRepStar :: forall (a :: *). !(TypeRep a) -> SomeTypeRepStar
 
 instance Typeable a => SingI (a :: *) where
-  sing = STypeRep
+  sing = STypeRep typeRep
 instance SingKind Type where
-  type Demote Type = TypeRep
-  fromSing (STypeRep :: Sing a) = typeOf (undefined :: a)
-  toSing = dirty_mk_STypeRep
+  type Demote Type = SomeTypeRepStar
+  fromSing (STypeRep tr) = SomeTypeRepStar tr
+  toSing (SomeTypeRepStar tr) = SomeSing $ STypeRep tr
 
 instance PEq Type where
   type (a :: *) :== (b :: *) = a == b
 
 instance SEq Type where
-  (STypeRep :: Sing a) %:== (STypeRep :: Sing b) =
-    case (eqT :: Maybe (a :~: b)) of
-      Just Refl -> STrue
-      Nothing   -> unsafeCoerce SFalse
+  STypeRep tra %:== STypeRep trb =
+    case eqTypeRep tra trb of
+      Just HRefl -> STrue
+      Nothing    -> unsafeCoerce SFalse
                     -- the Data.Typeable interface isn't strong enough
                     -- to enable us to define this without unsafeCoerce
 
 instance SDecide Type where
-  (STypeRep :: Sing a) %~ (STypeRep :: Sing b) =
-    case (eqT :: Maybe (a :~: b)) of
-      Just Refl -> Proved Refl
-      Nothing   -> Disproved (\Refl -> error "Data.Typeable.eqT failed")
+  STypeRep tra %~ STypeRep trb =
+    case eqTypeRep tra trb of
+      Just HRefl -> Proved Refl
+      Nothing    -> Disproved (\Refl -> error "Type.Reflection.eqTypeRep failed")
 
 -- TestEquality instance already defined, but we need this one:
 instance TestCoercion Sing where
-  testCoercion (STypeRep :: Sing a) (STypeRep :: Sing b) =
-    case (eqT :: Maybe (a :~: b)) of
-      Just Refl -> Just Coercion
-      Nothing   -> Nothing
-
--- everything below here is private and dirty. Don't look!
-
-newtype DI = Don'tInstantiate (forall a. Typeable a => Sing a)
-dirty_mk_STypeRep :: TypeRep -> SomeSing *
-dirty_mk_STypeRep rep =
-  let justLikeTypeable :: Proxy# a -> TypeRep
-      justLikeTypeable _ = rep
-  in
-  unsafeCoerce (Don'tInstantiate STypeRep) justLikeTypeable
+  testCoercion (STypeRep tra) (STypeRep trb) =
+    case eqTypeRep tra trb of
+      Just HRefl -> Just Coercion
+      Nothing    -> Nothing
