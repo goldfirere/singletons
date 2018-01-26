3b29fc8a87ef18d5cc00ba3e2355ea74104406b0
Ryan Scott <ryan.gl.scott@gmail.com>
2018-01-26 10:12:00 -0500

    Replace some occurrences of (TyFun a b -> Type) with (a ~> b)

--- tests/compile-and-dump/Promote/GenDefunSymbols.hs
+++ tests/compile-and-dump/Promote/GenDefunSymbols.hs
@@ -1,12 +1,12 @@
 module Promote.GenDefunSymbols where
 
-import Data.Singletons (Apply, TyFun)
+import Data.Singletons (Apply, type (~>))
 import Data.Singletons.SuppressUnusedWarnings
 import Data.Singletons.TH (genDefunSymbols)
 import GHC.TypeLits hiding (type (*))
 import Data.Kind (Type)
 
-type family LiftMaybe (f :: TyFun a b -> Type) (x :: Maybe a) :: Maybe b where
+type family LiftMaybe (f :: a ~> b) (x :: Maybe a) :: Maybe b where
     LiftMaybe f Nothing = Nothing
     LiftMaybe f (Just a) = Just (Apply f a)
 
