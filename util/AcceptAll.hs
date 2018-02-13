48224044d23e4b2acde14a1f67a8131d221c4571
Ryan Scott <ryan.gl.scott@gmail.com>
2018-02-13 18:20:10 -0500

    Cabalize accept-all
    This makes it easier to invoke the accept-all executable with
    `cabal new-run`.

--- util/AcceptAll.hs
+++ util/AcceptAll.hs
@@ -6,14 +6,15 @@ import Turtle
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
 
+main :: IO ()
 main = sh $ do
   (ExitSuccess, top_dir) <- shellStrict "git rev-parse --show-toplevel" empty
   test_dir  <- ls (fromText (Text.strip top_dir) </> "tests" </> "compile-and-dump")
   True <- testdir test_dir
   test_file <- ls test_dir
-  Right test_file <- return (toText test_file)
+  Right test_file' <- return (toText test_file)
   let pat = plus (notChar '.') <* (text ".actual")
-  name_base <- select (match pat test_file)
+  name_base <- select (match pat test_file')
 
   template_exists <- testfile (fromText $ name_base <> ".ghc84.template")
   when (not template_exists) $ do
