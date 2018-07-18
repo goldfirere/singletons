bbf936ff85763f07ec485ba1b702d41a11c8e874
Ryan Scott <ryan.gl.scott@gmail.com>
2018-07-18 12:34:13 -0400

    Follow-up regression test for #312
    In 21777c5755e10d539a1ea884454c6654993d38e2, I lamented the fact that
    I couldn't add a regression test involving scoped type variables in
    class methods due to Trac #14885. Luckily, that ticket has been
    fixed, and since we now require GHC 8.6 as the minimum, it is safe
    to assume that this fix is present.

--- tests/compile-and-dump/Singletons/T312.hs
+++ tests/compile-and-dump/Singletons/T312.hs
@@ -6,4 +6,9 @@ $(singletons [d|
   class Foo a where
     bar :: a -> b -> b
     bar _ x = x
+
+    baz :: forall b. a -> b -> b
+    baz = h where
+      h :: forall c. c -> b -> b
+      h _ x = x
   |])
