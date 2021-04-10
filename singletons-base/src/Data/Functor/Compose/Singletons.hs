d9db97cf03d6ceea6f796f931d6846e91125a819
Ryan Scott <ryan.gl.scott@gmail.com>
2021-04-09 21:24:37 -0400

    Add Data.Functor.{Compose,Product,Sum}.Singletons
    Fixes #491.

---
+++ singletons-base/src/Data/Functor/Compose/Singletons.hs
@@ -0,0 +1,95 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.Functor.Compose.Singletons
+-- Copyright   :  (C) 2021 Ryan Scott
+-- License     :  BSD-style (see LICENSE)
+-- Maintainer  :  Richard Eisenberg (rae@cs.brynmawr.edu)
+-- Stability   :  experimental
+-- Portability :  non-portable
+--
+-- Exports the promoted and singled versions of the 'Compose' data type.
+--
+-----------------------------------------------------------------------------
+
+module Data.Functor.Compose.Singletons (
+  -- * The 'Compose' singleton
+  Sing, SCompose(..), GetCompose, sGetCompose,
+
+  -- * Defunctionalization symbols
+  ComposeSym0, ComposeSym1,
+  GetComposeSym0, GetComposeSym1
+  ) where
+
+import Control.Applicative
+import Control.Applicative.Singletons
+import Data.Foldable.Singletons
+import Data.Functor.Compose
+import Data.Functor.Singletons
+import Data.Kind
+import Data.Singletons
+import Data.Singletons.TH
+import Data.Traversable.Singletons
+
+{-
+In order to keep the type arguments to Compose poly-kinded and with inferred
+specificities, we define the singleton version of Compose, as well as its
+defunctionalization symbols, by hand. This is very much in the spirit of the
+code in Data.Functor.Const.Singletons. (See the comments above SConst in that
+module for more details on this choice.)
+-}
+infixr 9 `SCompose`
+type SCompose :: Compose f g a -> Type
+data SCompose :: Compose f g a -> Type where
+  SCompose :: forall f g a (x :: f (g a)).
+              Sing x -> SCompose ('Compose @f @g @a x)
+type instance Sing = SCompose
+instance SingI x => SingI ('Compose x) where
+  sing = SCompose sing
+
+infixr 9 `ComposeSym0`
+type ComposeSym0 :: f (g a) ~> Compose f g a
+data ComposeSym0 z
+type instance Apply ComposeSym0 x = 'Compose x
+
+infixr 9 `ComposeSym1`
+type ComposeSym1 :: f (g a) -> Compose f g a
+type family ComposeSym1 x where
+  ComposeSym1 x = 'Compose x
+
+$(singletonsOnly [d|
+  getCompose :: Compose f g a -> f (g a)
+  getCompose (Compose x) = x
+
+  instance (Functor f, Functor g) => Functor (Compose f g) where
+      fmap f (Compose x) = Compose (fmap (fmap f) x)
+      a <$ (Compose x) = Compose (fmap (a <$) x)
+
+  instance (Foldable f, Foldable g) => Foldable (Compose f g) where
+      foldMap f (Compose t) = foldMap (foldMap f) t
+
+  instance (Traversable f, Traversable g) => Traversable (Compose f g) where
+      traverse f (Compose t) = Compose <$> traverse (traverse f) t
+
+  instance (Applicative f, Applicative g) => Applicative (Compose f g) where
+      pure x = Compose (pure (pure x))
+      Compose f <*> Compose x = Compose (liftA2 (<*>) f x)
+      liftA2 f (Compose x) (Compose y) =
+        Compose (liftA2 (liftA2 f) x y)
+
+  instance (Alternative f, Applicative g) => Alternative (Compose f g) where
+      empty = Compose empty
+      Compose x <|> Compose y = Compose (x <|> y)
+  |])
---
+++ singletons-base/src/Data/Functor/Sum/Singletons.hs
@@ -0,0 +1,94 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.Functor.Sum.Singletons
+-- Copyright   :  (C) 2021 Ryan Scott
+-- License     :  BSD-style (see LICENSE)
+-- Maintainer  :  Richard Eisenberg (rae@cs.brynmawr.edu)
+-- Stability   :  experimental
+-- Portability :  non-portable
+--
+-- Exports the promoted and singled versions of the 'Sum' data type.
+--
+-----------------------------------------------------------------------------
+
+module Data.Functor.Sum.Singletons (
+  -- * The 'Product' singleton
+  Sing, SSum(..),
+
+  -- * Defunctionalization symbols
+  InLSym0, InLSym1,
+  InRSym0, InRSym1,
+  ) where
+
+import Data.Foldable.Singletons hiding (Sum)
+import Data.Functor.Singletons
+import Data.Functor.Sum
+import Data.Kind
+import Data.Singletons
+import Data.Singletons.TH
+import Data.Traversable.Singletons
+
+{-
+In order to keep the type arguments to Sum poly-kinded and with inferred
+specificities, we define the singleton version of Sum, as well as its
+defunctionalization symbols, by hand. This is very much in the spirit of the
+code in Data.Functor.Const.Singletons. (See the comments above SConst in that
+module for more details on this choice.)
+-}
+type SSum :: Sum f g a -> Type
+data SSum :: Sum f g a -> Type where
+  SInL :: forall f g a (x :: f a).
+          Sing x -> SSum ('InL @f @g @a x)
+  SInR :: forall f g a (y :: g a).
+          Sing y -> SSum ('InR @f @g @a y)
+type instance Sing = SSum
+instance SingI x => SingI ('InL x) where
+  sing = SInL sing
+instance SingI y => SingI ('InR y) where
+  sing = SInR sing
+
+type InLSym0 :: forall f g a. f a ~> Sum f g a
+data InLSym0 z
+type instance Apply InLSym0 x = 'InL x
+
+type InLSym1 :: forall f g a. f a -> Sum f g a
+type family InLSym1 x where
+  InLSym1 x = 'InL x
+
+type InRSym0 :: forall f g a. g a ~> Sum f g a
+data InRSym0 z
+type instance Apply InRSym0 y = 'InR y
+
+type InRSym1 :: forall f g a. g a -> Sum f g a
+type family InRSym1 x where
+  InRSym1 y = 'InR y
+
+$(singletonsOnly [d|
+  instance (Functor f, Functor g) => Functor (Sum f g) where
+      fmap f (InL x) = InL (fmap f x)
+      fmap f (InR y) = InR (fmap f y)
+
+      a <$ (InL x) = InL (a <$ x)
+      a <$ (InR y) = InR (a <$ y)
+
+  instance (Foldable f, Foldable g) => Foldable (Sum f g) where
+      foldMap f (InL x) = foldMap f x
+      foldMap f (InR y) = foldMap f y
+
+  instance (Traversable f, Traversable g) => Traversable (Sum f g) where
+      traverse f (InL x) = InL <$> traverse f x
+      traverse f (InR y) = InR <$> traverse f y
+  |])
